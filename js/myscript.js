0031b0909728ab5412059aa3a1630d7a8d969fa3
G-Lumia <giorgiolumia@gmail.com>
2023-04-03 13:49:07 +0200

    added last mesage to left side

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,3 @@
-
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 // Milestone 5 - opzionale
 // Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
 
@@ -222,7 +220,7 @@ createApp
                 return this.contacts.filter((contact) =>
                 contact.name.trim().toLowerCase().includes(this.search.trim().toLowerCase()));
             }
-        }
+        },
     },
     mounted(){
 
